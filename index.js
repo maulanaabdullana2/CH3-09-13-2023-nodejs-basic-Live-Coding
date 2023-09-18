4dbe969d97d97fb7e82a28179c474f81a3a5a3e4
maulanaabdullana2 <maulanaabdullana123@gmail.com>
2023-09-18 22:10:12 +0700

    Node js

--- index.js
+++ index.js
@@ -1,37 +1,78 @@
-const fs = require('fs')
-const http = require('http')
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+// SERVER dengan HTTP
+const replaceTemplate = (template, product) => {
+    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const overviewPage = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
+const productTemplate = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
+const productCardTemplate = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url
-    if(pathName === '/hello'){
-        res.end('ini hello fsw 2')
-    }else if(pathName === '/product'){
-        res.end(JSON.stringify({
-          data: 'Hello Fsw 2',
-        }));
-    }else if(pathName === '/api'){
-        const data = fs.readFileSync(`${__dirname}/dev-data/data.json`)
-        res.writeHead(200,{
-            'Content-type':'application/json'
+    const { pathname: pathName, query } = url.parse(req.url, true);
+    console.log(pathName)
+    console.log(query)
+
+    // HELLO PAGE 
+    if (pathName === '/hello') {
+        res.end('ini hello ke FSW 2')
+
+        // simple api
+    } else if (pathName === '/api') {
+        res.writeHead(200, {
+            'Content-type': 'application/json'
+        });
+        res.end(data);
+
+        // overview page 
+    } else if (pathName === '/overview') {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
         })
-        res.end(data)
-    }else if(pathName === '/overview'){
-        const overviewPage = fs.readFileSync(`${__dirname}/templates/overview.html`)
-        res.writeHead(200,{
-            'Content-type':'text/html'
+
+        const productCardsHtml = dataObj.map(el => replaceTemplate(productCardTemplate, el));
+        const output = overviewPage.replace('{%PRODUCT_CARDS%', productCardsHtml);
+
+        res.end(output);
+
+
+        //  product page
+    } else if (pathName === '/product') {
+        console.log(query)
+        res.writeHead(200, {
+            'Content-type': 'text/html'
         })
-        res.end(overviewPage)
-    }else{
-        res.writeHead(404,{
-            'Content-type':'text/html'
+        const product = dataObj[query.id];
+        const output = replaceTemplate(productTemplate, product);
+        res.end(output);
+        
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html'
         })
-        res.end('<h1>Url Ini Gak Ada Apa 2</h1')
+        res.end('<h1>url ini gak ada apa2 bestie!!!</h1>');
     }
-  });
-  
-  server.listen(8000,'127.0.0.1',()=>{
-    console.log('Sever Running');
-  });
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Hello Server nya Jalann!!!');
+});
 
 
 //Blocking esecution atau (sychronous)
